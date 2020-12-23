5317fa880932af6e8d6c50f7a810f5f6977cd262
Ryouyashi <75989894+Ryouyashi@users.noreply.github.com>
2020-12-23 11:09:42 +0000

    build-index

--- build-tools/build.bat
+++
@@ -1,8 +0,0 @@
-npm run build-full
-# I made .bat files to run the client and the server 
-# so I don't need to do any extra steps to test something.
-start client.bat 
-start server.bat
-
-#Once it is done, then exit
-exit
